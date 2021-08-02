b7481e1e3f2c446c42c58ee986cad5bd3a5d298c
Dionisio Chavez <dionisiochavez@Dionisios-MacBook-Pro.local>
2021-08-02 09:50:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Form from './tutorial/00-forms'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ShortCircuirEvaluation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShortCircuirEvaluation(props) = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ShortCircuirEvaluation;
